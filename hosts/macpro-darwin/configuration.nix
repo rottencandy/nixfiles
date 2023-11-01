dba042742e15282742d1cd2d343ed044bf85813f
Mohammed Saud <md.saud020@gmail.com>
2023-11-01 14:26:06 +0530

    add sketchybar, manual yabai & skhd configs
    nix-darwin managed configs have issues

--- hosts/macpro-darwin/configuration.nix
+++ hosts/macpro-darwin/configuration.nix
@@ -19,12 +19,16 @@
     extraOptions = "experimental-features = nix-command flakes";
   };
 
-  # Auto upgrade nix package and the daemon service.
-  services.nix-daemon.enable = true;
-
   system = {
     defaults = {
-      dock.autohide = true;
+      dock = {
+        autohide = true;
+        mru-spaces = false;
+        wvous-tl-corner = 1;
+        wvous-tr-corner = 1;
+        wvous-bl-corner = 1;
+        wvous-br-corner = 1;
+      };
       trackpad = {
         Clicking = true;
         Dragging = true;
@@ -34,7 +38,11 @@
         AppleInterfaceStyle = "Dark";
         InitialKeyRepeat = 14;
         KeyRepeat = 1;
+        _HIHideMenuBar = true;
       };
+
+      # required for yabai
+      spaces.spans-displays = false;
     };
 
     keyboard = {
@@ -83,10 +91,102 @@
       autoUpdate = true;
       cleanup = "uninstall";
     };
+    taps = [
+      "koekeishiya/formulae"
+    ];
+    brews = [
+      "koekeishiya/formulae/yabai"
+    ];
   };
 
   programs.gnupg.agent = {
     enable = true;
     enableSSHSupport = true;
+  };
+
+  # Auto upgrade nix package and the daemon service.
+  services.nix-daemon.enable = true;
+
+  services.sketchybar = {
+    enable = true;
+    config = ''
+PLUGIN_DIR="$CONFIG_DIR/plugins"
+
+##### Bar Appearance #####
+sketchybar --bar height=32        \
+                 blur_radius=30   \
+                 position=top     \
+                 sticky=off       \
+                 padding_left=10  \
+                 padding_right=10 \
+                 color=0x15ffffff
+
+##### Changing Defaults #####
+sketchybar --default icon.font="FiraCode Nerd Font Mono:Bold:17.0"  \
+                     icon.color=0xffffffff                 \
+                     label.font="FiraCode Nerd Font Mono:Bold:14.0" \
+                     label.color=0xffffffff                \
+                     padding_left=5                        \
+                     padding_right=5                       \
+                     label.padding_left=4                  \
+                     label.padding_right=4                 \
+                     icon.padding_left=4                   \
+                     icon.padding_right=4
+
+##### Adding Mission Control Space Indicators #####
+SPACE_ICONS=("1" "2" "3" "4" "5" "6" "7" "8" "9" "10")
+
+for i in "''${!SPACE_ICONS[@]}"
+do
+  sid=$(($i+1))
+  sketchybar --add space space.$sid left                                 \
+             --set space.$sid space=$sid                                 \
+                              icon=''${SPACE_ICONS[i]}                     \
+                              background.color=0x44ffffff                \
+                              background.corner_radius=5                 \
+                              background.height=20                       \
+                              background.drawing=off                     \
+                              label.drawing=off                          \
+                              script="$PLUGIN_DIR/space.sh"              \
+                              click_script="yabai -m space --focus $sid"
+done
+
+##### Adding Left Items #####
+sketchybar --add item space_separator left                         \
+           --set space_separator icon=                            \
+                                 padding_left=10                   \
+                                 padding_right=10                  \
+                                 label.drawing=off                 \
+                                                                   \
+           --add item front_app left                               \
+           --set front_app       script="$PLUGIN_DIR/front_app.sh" \
+                                 icon.drawing=off                  \
+           --subscribe front_app front_app_switched
+
+##### Adding Right Items #####
+# Some items refresh on a fixed cycle, e.g. the clock runs its script once
+# every 10s. Other items respond to events they subscribe to, e.g. the
+# volume.sh script is only executed once an actual change in system audio
+# volume is registered. More info about the event system can be found here:
+# https://felixkratz.github.io/SketchyBar/config/events
+
+sketchybar --add item clock right                              \
+           --set clock   update_freq=10                        \
+                         icon=                                \
+                         script="$PLUGIN_DIR/clock.sh"         \
+
+           #                                                    \
+           #--add item volume right                             \
+           #--set volume  script="$PLUGIN_DIR/volume.sh"        \
+           #--subscribe volume volume_change                    \
+           #                                                    \
+           #--add item battery right                            \
+           #--set battery script="$PLUGIN_DIR/battery.sh"       \
+           #              update_freq=120                       \
+           #--subscribe battery system_woke power_source_change
+
+##### Finalizing Setup #####
+sketchybar --update
+    '';
   };
 }
--- hosts/macpro-darwin/home.nix
+++ hosts/macpro-darwin/home.nix
@@ -49,15 +49,18 @@ in
     zathura
     #cmus
     #freetube
+    #uxn
+    mpv
 
     # mac
-    karabiner-elements
+    #karabiner-elements
     #goku
-    sketchybar
-    skhd
     m-cli
     iina
     monitorcontrol
+    aldente
+    skhd
+    unnaturalscrollwheels
 
     # utils
     git
@@ -68,6 +71,8 @@ in
     ffmpeg
     pandoc
     todo-txt-cli
+    wgcf
+    ffsend
 
     macchina
     bottom
@@ -76,6 +81,8 @@ in
     lsd
     jq
     jless
+    jo
+    fastgron
     ripgrep
     xh
     fd
@@ -83,6 +90,7 @@ in
     nnn
     yazi
     broot
+    ripdrag
     moreutils
 
     yt-dlp
@@ -119,6 +127,59 @@ in
       recursive = true;
     };
     ".todo.cfg".source = ../../home/todo.cfg;
+    ".config/skhd/skhdrc".text = ''
+      cmd - return : wezterm
+      cmd + shift - r : yabai -restart-service
+
+      cmd - h : yabai -m window --focus west
+      cmd - j : yabai -m window --focus south
+      cmd - k : yabai -m window --focus north
+      cmd - l : yabai -m window --focus east
+
+      cmd + shift - h : yabai -m window --warp west
+      cmd + shift - j : yabai -m window --warp south
+      cmd + shift - k : yabai -m window --warp north
+      cmd + shift - l : yabai -m window --warp east
+
+      # requires scripting addition
+      cmd - 1 : yabai -m space --focus 1
+      cmd - 2 : yabai -m space --focus 2
+      cmd - 3 : yabai -m space --focus 3
+      cmd - 4 : yabai -m space --focus 4
+      cmd - 5 : yabai -m space --focus 5
+
+      # monitor focus, requires display number
+      cmd + ctrl - l : yabai -m display --focus 1
+      cmd + ctrl - h : yabai -m display --focus 2
+
+      cmd + shift - 1 : yabai -m window --space 1; yabai -m space --focus 1
+      cmd + shift - 2 : yabai -m window --space 2; yabai -m space --focus 2
+      cmd + shift - 3 : yabai -m window --space 3; yabai -m space --focus 3
+      cmd + shift - 4 : yabai -m window --space 4; yabai -m space --focus 4
+      cmd + shift - 5 : yabai -m window --space 5; yabai -m space --focus 5
+
+      cmd - m : yabai -m window --toggle zoom-parent
+      cmd - f : yabai -m window --toggle zoom-fullscreen
+      cmd - v : yabai -m window --toggle split
+      cmd + shift - space : yabai -m window --toggle float --grid 4:4:1:1:2:2
+    '';
+    ".config/yabai/yabairc" = {
+      text = ''
+        #!/usr/bin/env sh
+
+        yabai -m config layout bsp
+        yabai -m config top_padding 16
+        yabai -m config bottom_padding 16
+        yabai -m config left_padding 16
+        yabai -m config right_padding 16
+        yabai -m config window_gap 8
+        #yabai -m config focus_follows_mouse autoraise
+        yabai -m config mouse_follows_focus on
+        yabai -m config mouse_modifier cmd
+        yabai -m config external_bar all:32:0
+      '';
+      executable = true;
+    };
   };
 
   home.sessionVariables = {
@@ -145,7 +206,7 @@ in
 
     shellAliases = {
       # TODO get advcpmv to nixpkgs
-      gd = "DELTA_NAVIGATE = 1 git diff";
+      gd = "DELTA_NAVIGATE=1 git diff";
       gr = "cd ./$(git rev-parse --show-cdup)";
       k = "kubectl";
       l = "ls -CF";
