c14a4c9f859c97db257b18e37ff3d8c31151014e
Mohammed Saud <md.saud020@gmail.com>
2023-11-22 00:05:37 +0530

    use git module config

--- hosts/asus-linux/home.nix
+++ hosts/asus-linux/home.nix
@@ -10,28 +10,6 @@ let
     exec -a "$0" "$@"
   '';
 
-  git-glog = pkgs.writeShellScriptBin "glog" ''
-    setterm -linewrap off
-
-    git --no-pager log --all --color=always --graph --abbrev-commit --decorate \
-        --format=format:'%C(bold blue)%h%C(reset) - %C(bold green)(%ar)%C(reset) %s%C(reset) %C(dim white)- %an%C(reset)%C(bold yellow)%d%C(reset)' | \
-        sed -E \
-        -e 's/\|(\x1b\[[0-9;]*m)+\\(\x1b\[[0-9;]*m)+ /├\1─_\2/' \
-        -e 's/(\x1b\[[0-9;]+m)\|\x1b\[m\1\/\x1b\[m /\1├─_\x1b\[m/' \
-        -e 's/\|(\x1b\[[0-9;]*m)+\\(\x1b\[[0-9;]*m)+/├\1_\2/' \
-        -e 's/(\x1b\[[0-9;]+m)\|\x1b\[m\1\/\x1b\[m/\1├_\x1b\[m/' \
-        -e 's/_(\x1b\[[0-9;]*m)+\\/_\1__/' \
-        -e 's/_(\x1b\[[0-9;]*m)+\//_\1__/' \
-        -e 's/(\||\\)\x1b\[m   (\x1b\[[0-9;]*m)/__\2/' \
-        -e 's/(\x1b\[[0-9;]*m)\\/\1_/g' \
-        -e 's/(\x1b\[[0-9;]*m)\//\1_/g' \
-        -e 's/^\*|(\x1b\[m )\*/\1_/g' \
-        -e 's/(\x1b\[[0-9;]*m)\|/\1│/g' \
-        | command less -r +'/[^/]HEAD'
-
-    setterm -linewrap on
-  '';
-
   # Grab a password from the password store into the clipboard using fzf
   pass-get = pkgs.writeShellScriptBin "getp" ''
     PASS_DIR=~/.password-store
@@ -51,6 +29,7 @@ in
   imports = [
     ../../modules/bash
     ../../modules/tmux
+    ../../modules/git.nix
     ../../modules/hyprland
   ];
   home.username = "saud";
@@ -123,7 +102,6 @@ in
     winetricks
 
     # shell functions
-    git-glog
     pass-get
 
     # applications
@@ -318,81 +296,6 @@ in
   #    "!.git"
   #  ];
   #};
-
-  programs.git = {
-    enable = true;
-    delta = {
-      enable = true;
-      options = {
-        "side-by-side" = true;
-	"line-numbers" = true;
-	navigate = true;
-      };
-    };
-    userName = "Mohammed Saud";
-    userEmail = "md.saud020@gmail.com";
-    aliases = {
-      st = "status";
-      co = "checkout";
-      br = "branch";
-      last = "log -1 HEAD";
-      yoink = "reset --hard";
-      # All unmerged commits after fetch
-      lc = "log ORIG_HEAD.. --stat --no-merges";
-
-      # log in local time
-      llog = "log --date=local";
-
-      # Fetch PR from upstream
-      pr = "!f() { git fetch upstream pull/\${1}/head:pr\${1}; }; f";
-
-      # Pretty log
-      lg = "log --graph --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset' --abbrev-commit --date=relative";
-
-      # Show current branch
-      pwd = "symbolic-ref --short HEAD";
-      # Show corresponding upstream branch
-      upstream = "name-rev @{upstream}";
-
-      # Set upstream before pushing if necessary
-      pu = "!f() { if [ \"$(git upstream 2> /dev/null)\" ]; then git push; else git push -u origin $(git pwd); fi }; f";
-
-      # Pull submodules
-      pulsub = "submodule update --remote";
-    };
-    extraConfig = {
-      # colorize output
-      color = {
-        diff = "auto";
-        status = "auto";
-        branch = "auto";
-        interactive = "auto";
-        ui = true;
-        pager = true;
-      };
-      # cache credentials
-      credential = {
-        helper = "cache --timeout=3600";
-      };
-      merge = {
-        conflictstyle = "diff3";
-      };
-      sendemail = {
-        smtpEncryption = "tls";
-        smtpServer = "smtp.gmail.com";
-        smtpServerPort = "587";
-        smtpUser = "md.saud020@gmail.com";
-      };
-      pull.rebase = true;
-      filter.lfs = {
-        clean = "git-lfs clean -- %f";
-        smudge = "git-lfs smudge -- %f";
-        process = "git-lfs filter-process";
-        required = true;
-      };
-      init.defaultBranch = "main";
-    };
-  };
 
   programs.wezterm = {
     enable = true;
