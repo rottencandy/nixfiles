9ca6ab2a523261a69f1a4b7d8a1205ea15d2fccd
saud <md.saud020@gmail.com>
2024-05-03 22:32:13 +0530

    avoid hiding mouse with wezterm

--- modules/wezterm.nix
+++ modules/wezterm.nix
@@ -16,6 +16,7 @@
         font_size = 10.0,
         color_scheme = 'Earthsong',
         hide_tab_bar_if_only_one_tab = true,
+        hide_mouse_cursor_when_typing = false,
         window_decorations = "RESIZE",
       }
     '';
