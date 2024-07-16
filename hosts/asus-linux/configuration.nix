e8fde4e6db654757ec8bce0f9e9f21d4c80cc21e
saud <md.saud020@gmail.com>
2024-07-16 19:20:49 +0530

    update, inputs fix build

--- hosts/asus-linux/configuration.nix
+++ hosts/asus-linux/configuration.nix
@@ -156,8 +156,6 @@ in
   # Enable CUPS to print documents.
   #services.printing.enable = true;
 
-  # Enable sound with pipewire.
-  sound.enable = true;
   hardware.pulseaudio.enable = false;
   security.rtkit.enable = true;
   services.pipewire = {
