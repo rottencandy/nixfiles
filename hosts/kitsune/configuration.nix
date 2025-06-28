316f1e6b98bf55b1228bfc296c5fd1a35406ee87
Mohammed Saud <md.saud020@gmail.com>
2025-06-28 18:19:13 +0530

    finalize kitsune config

--- hosts/kitsune/configuration.nix
+++ hosts/kitsune/configuration.nix
@@ -1,9 +1,8 @@
-{
-  config,
-  pkgs,
-  lib,
-  ...
-}:
+# Edit this configuration file to define what should be installed on
+# your system.  Help is available in the configuration.nix(5) man page
+# and in the NixOS manual (accessible by running ‘nixos-help’).
+
+{ config, pkgs, ... }:
 
 let
   # bash script to let dbus know about important env variables and
@@ -50,17 +49,16 @@ in
   imports = [
     # Include the results of the hardware scan.
     ./hardware-configuration.nix
-    #../../modules/syncthing.nix
   ];
 
   # Bootloader.
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
 
-  # Load AMD directly at boot
-  boot.initrd.kernelModules = [ "amdgpu" ];
+  # Use latest kernel.
+  boot.kernelPackages = pkgs.linuxPackages_latest;
 
-  networking.hostName = "wyvern"; # Define your hostname.
+  networking.hostName = "kitsune"; # Define your hostname.
   # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
 
   # Configure network proxy if necessary
@@ -73,26 +71,15 @@ in
   # Set your time zone.
   time.timeZone = "Asia/Kolkata";
 
-  # Enable the X11 windowing system
+  # Enable the X11 windowing system.
   services.xserver.enable = true;
   # but don't run a display manager by default
   services.xserver.autorun = false;
-  # Use NVIDIA driver (required for PRIME offload)
-  services.xserver.videoDrivers = [ "nvidia" ];
-
-  # Enable the KDE Plasma Desktop Environment.
-  #services.displayManager.sddm = {
-  #  enable = true;
-  #  wayland.enable = true;
-  #};
-  services.desktopManager.plasma6.enable = true;
   services.xserver.displayManager.startx.enable = true;
 
-  # Enable touchpad support (enabled default in most desktopManager).
-  services.libinput = {
-    enable = true;
-    touchpad.naturalScrolling = true;
-  };
+  # Enable the GNOME Desktop Environment.
+  #services.displayManager.gdm.enable = true;
+  services.desktopManager.gnome.enable = true;
 
   # Configure keymap in X11
   services.xserver.xkb = {
@@ -100,67 +87,10 @@ in
     variant = "";
   };
 
-  # Enable opengl
-  hardware.graphics = {
-    enable = true;
-    enable32Bit = true;
-    extraPackages = with pkgs; [
-      # Allow programs to optionally choose amdvlk
-      amdvlk
-      # trying to fix `WLR_RENDERER=vulkan sway`
-      vulkan-validation-layers
-    ];
-  };
-
-  hardware.nvidia = {
-    # Needed for most wayland compositors
-    modesetting.enable = true;
-    # Use proprietary module
-    open = false;
-    # Enable settings menu
-    nvidiaSettings = true;
-    # Use stable driver package
-    # Optionally, you may need to select the appropriate driver version for your specific GPU.
-    package = config.boot.kernelPackages.nvidiaPackages.beta;
-    #package = config.boot.kernelPackages.nvidiaPackages.mkDriver {
-    #  version = "550.67";
-    #  sha256_64bit = "sha256-mSAaCccc/w/QJh6w8Mva0oLrqB+cOSO1YMz1Se/32uI=";
-    #  sha256_aarch64 = "sha256-+UuK0UniAsndN15VDb/xopjkdlc6ZGk5LIm/GNs5ivA=";
-    #  openSha256 = "sha256-M/1qAQxTm61bznAtCoNQXICfThh3hLqfd0s1n1BFj2A=";
-    #  settingsSha256 = "sha256-FUEwXpeUMH1DYH77/t76wF1UslkcW721x9BHasaRUaM=";
-    #  persistencedSha256 = "sha256-ojHbmSAOYl3lOi2X6HOBlokTXhTCK6VNsH6+xfGQsyo=";
-    #  ibtSupport = true;
-    #};
-    #package = config.boot.kernelPackages.nvidiaPackages.mkDriver {
-    #  version = "550.76";
-    #  sha256_64bit = "sha256-2/wAmBNiePhX74FsV7j21LnCaubxMC/kAYMmf8kQt1s=";
-    #  sha256_aarch64 = "sha256-LhiyYCUTFwqzUITK6CKIqxOQp62wG1RuKOuP0fTKoVk=";
-    #  openSha256 = "sha256-RWaUXIr/yCRmX4yIyUxvBxrKCLKRKSi4lQJAYvrd2Kg=";
-    #  settingsSha256 = "sha256-Lv95+0ahvU1+X/twzWWVqQH4nqq491ALigH9TVBn+YY=";
-    #  persistencedSha256 = "sha256-rbgI9kGdVzGlPNEvaoOq2zrAMx+H8D+XEBah2eqZzuI=";
-    #};
-
-    # Enable prime offloading
-    prime = {
-      offload = {
-        enable = true;
-        enableOffloadCmd = true;
-      };
-      allowExternalGpu = true;
-      nvidiaBusId = "PCI:1:0:0"; # lspci | grep VGA | grep NVIDIA
-      amdgpuBusId = "PCI:5:0:0"; # lspci | grep VGA | grep AMD
-    };
-    powerManagement.finegrained = true;
-  };
-
-  # Enable qmk hardware support for a normal non-root user
-  hardware.keyboard = {
-    qmk.enable = true;
-  };
-
   # Enable CUPS to print documents.
-  #services.printing.enable = true;
+  services.printing.enable = true;
 
+  # Enable sound with pipewire.
   services.pulseaudio.enable = false;
   security.rtkit.enable = true;
   services.pipewire = {
@@ -169,13 +99,21 @@ in
     alsa.support32Bit = true;
     pulse.enable = true;
     # If you want to use JACK applications, uncomment this
-    jack.enable = true;
+    #jack.enable = true;
 
     # use the example session manager (no others are packaged yet so this is enabled by default,
     # no need to redefine it in your config for now)
     #media-session.enable = true;
   };
-  hardware.bluetooth.enable = true;
+
+  # Enable touchpad support (enabled default in most desktopManager).
+  services.libinput = {
+    enable = true;
+    touchpad = {
+      naturalScrolling = true;
+      tapping = true;
+    };
+  };
 
   # Define a user account. Don't forget to set a password with ‘passwd’.
   users.users.saud = {
@@ -184,23 +122,20 @@ in
     extraGroups = [
       "networkmanager"
       "wheel"
-      "video"
-      "libvirtd"
-      "adbusers"
-      "dialout"
     ];
-    #packages = with pkgs; [
-    #  thunderbird
-    #];
+    packages = with pkgs; [
+      #  thunderbird
+    ];
   };
 
-  nixpkgs = {
-    hostPlatform = "x86_64-linux";
-    config = {
-      allowUnfree = true;
-    };
-    overlays = [ ];
+  # Install firefox.
+  programs.firefox = {
+    enable = true;
+    nativeMessagingHosts.packages = [ pkgs.tridactyl-native ];
   };
+
+  # Allow unfree packages
+  nixpkgs.config.allowUnfree = true;
 
   nix = {
     package = pkgs.nixVersions.stable;
@@ -244,8 +179,7 @@ in
   security.polkit.enable = true;
   security.pam.services.swaylock = { };
 
-  # workaround for https://github.com/NixOS/nixpkgs/issues/414135
-  security.lsm = lib.mkForce [ ];
+  programs.adb.enable = true;
 
   # sunshine server
   services.sunshine = {
@@ -255,93 +189,60 @@ in
     openFirewall = true;
   };
 
-  # xdg-desktop-portal works by exposing a series of D-Bus interfaces
-  # known as portals under a well-known name
-  # (org.freedesktop.portal.Desktop) and object path
-  # (/org/freedesktop/portal/desktop).
-  # The portal interfaces include APIs for file access, opening URIs,
-  # printing and others.
-  services.dbus.enable = true;
-  xdg.portal = {
+  # TLP
+  services.tlp = {
     enable = true;
-    wlr.enable = true;
-    # gtk portal needed to make gtk apps happy (also needed for flatpak)
-    extraPortals = [ pkgs.xdg-desktop-portal-gtk ];
-    # xdg-desktop-portal 1.17 reworked how portal implementations are loaded
-    # we need to specify which portal backend t o use for the requested interface
-    # this option simply keeps behaviour the same as < 1.17, i.e.. use the first
-    # portal implementation found in lexicographical order
-    config.common.default = "*";
-  };
+    settings = {
+      CPU_SCALING_GOVERNOR_ON_AC = "performance";
+      CPU_SCALING_GOVERNOR_ON_BAT = "powersave";
 
-  # key daemon
-  # https://github.com/rvaiya/keyd/blob/master/keyd.service
-  systemd.services.keyd = {
-    enable = false;
-    requires = [ "local-fs.target" ];
-    after = [ "local-fs.target" ];
-    wantedBy = [ "sysinit.target" ];
-    serviceConfig = {
-      Type = "simple";
-      ExecStart = "${pkgs.keyd}/bin/keyd";
+      # mimic power-profiles-daemon
+      PLATFORM_PROFILE_ON_AC = "performance";
+      PLATFORM_PROFILE_ON_BAT = "balanced";
+      CPU_ENERGY_PERF_POLICY_ON_AC = "performance";
+      CPU_ENERGY_PERF_POLICY_ON_BAT = "balance_performance";
+      CPU_BOOST_ON_AC = 1;
+      CPU_BOOST_ON_BAT = 0;
+
+      #CPU_MIN_PERF_ON_AC = 0;
+      #CPU_MAX_PERF_ON_AC = 100;
+      #CPU_MIN_PERF_ON_BAT = 0;
+      #CPU_MAX_PERF_ON_BAT = 20;
+
+      #START_CHARGE_THRESH_BAT0 = 65; # 75 and below it starts to charge
+      STOP_CHARGE_THRESH_BAT0 = 70; # 80 and above it stops charging
     };
   };
-  environment.etc."keyd/default.conf".text = ''
-    [ids]
-    *
+  # this conflicts with tlp
+  services.power-profiles-daemon.enable = false;
 
-    [main]
-    # Maps capslock to escape when pressed and control when held.
-    capslock = overload(control, esc)
+  # Enable virtualisation
+  virtualisation.libvirtd.enable = true;
+  programs.virt-manager.enable = true;
 
-    # Remaps the escape key to capslock
-    esc = capslock
+  # Enable common container config files in /etc/containers
+  virtualisation.containers.enable = true;
+  virtualisation.podman = {
+    enable = true;
+    # Create a `docker` alias for podman, to use it as a drop-in replacement
+    #dockerCompat = true;
 
-    # Remaps print to second meta(super) key
-    compose = rightmeta
-    sysrq = rightmeta
-  '';
-
-  # steam client
-  #programs.steam = {
-  #  enable = true;
-  #  remotePlay.openFirewall = true; # Open ports in the firewall for Steam Remote Play
-  #  dedicatedServer.openFirewall = true; # Open ports in the firewall for Source Dedicated Server
-  #};
+    # Required for containers under podman-compose to be able to talk to each other.
+    defaultNetwork.settings.dns_enabled = true;
+  };
 
   # Some programs need SUID wrappers, can be configured further or are
   # started in user sessions.
-  # programs.mtr.enable = true;
-  # programs.gnupg.agent = {
-  #   enable = true;
-  #   enableSSHSupport = true;
-  # };
-
   programs.mtr.enable = true;
   programs.gnupg.agent = {
     enable = true;
     enableSSHSupport = true;
   };
 
-  programs.firefox = {
-    enable = true;
-    nativeMessagingHosts.packages = [ pkgs.tridactyl-native ];
-  };
-
   # List services that you want to enable:
-
-  # Enable flatpak
-  #services.flatpak.enable = true;
-
-  # Enable asus utils
-  services.asusd.enable = true;
-  #services.supergfxd.enable = true;
 
   # Enable the OpenSSH daemon.
   # services.openssh.enable = true;
-
-  # Enable upower for battery monitoring.
-  services.upower.enable = true;
 
   # Open ports in the firewall.
   networking.firewall = {
@@ -388,31 +289,14 @@ in
     ];
   };
 
-  # android tools
-  programs.adb.enable = true;
-
-  # Enable virtualisation
-  virtualisation.libvirtd.enable = true;
-  programs.virt-manager.enable = true;
-
-  # Enable common container config files in /etc/containers
-  virtualisation.containers.enable = true;
-  virtualisation.podman = {
-    enable = true;
-    # Create a `docker` alias for podman, to use it as a drop-in replacement
-    #dockerCompat = true;
-
-    # Required for containers under podman-compose to be able to talk to each other.
-    defaultNetwork.settings.dns_enabled = true;
-  };
-
   # This value determines the NixOS release from which the default
   # settings for stateful data, like file locations and database versions
   # on your system were taken. It‘s perfectly fine and recommended to leave
   # this value at the release version of the first install of this system.
   # Before changing this value read the documentation for this option
   # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
-  system.stateVersion = "23.05"; # Did you read the comment?
+  system.stateVersion = "25.05"; # Did you read the comment?
+
 }
 
 # vim: fdm=marker:fdl=0:et:sw=2
